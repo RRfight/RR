dec1938045a940d42908de79ff6eb2cfba97a77a
RRfight <597593585@qq.com>
2020-08-21 21:40:25 +0800

    这是我的第7 次提交

--- JavaProject/src/com/test/ClassFour.java
+++ JavaProject/src/com/test/ClassFour.java
@@ -3,6 +3,6 @@ package com.test;
 public class ClassFour {
     public static void main(String[] args) {
         System.out.println("11");
-        System.out.println("222");
+        System.out.println("222");jj
     }
 }
