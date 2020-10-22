3aca6f1bf818a6c93d057d9db250254f388e25ba
RRfight <597593585@qq.com>
2020-10-22 19:29:10 +0800

    cherry pick 1

--- JavaProject/src/com/rebasetest/C3.java
+++ JavaProject/src/com/rebasetest/C3.java
@@ -3,5 +3,6 @@ package com.rebasetest;
 public class C3 {
     public static void main(String[] args) {
         System.out.println("C3");
+        System.out.println("prepare for merge");
     }
 }
